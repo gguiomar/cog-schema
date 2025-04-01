5232466fefc4d45038127676d6dce909a97a339a
DanielGaivaoLozano <daniel@gaivao.net>
2025-04-01 16:42:36 +0200

    Deleted logs, old, benchmark_plots content. Added logs and benchmark_plots to gitignore

--- old/scratch/backup/snapshot_get.py
+++
@@ -1,9 +0,0 @@
-#%% download model files
-
-from huggingface_hub import snapshot_download
-
-repo_id = "marcelbinz/Llama-3.1-Centaur-8B"
-local_folder = "./llama_centaur_adapter"
-snapshot_download(repo_id=repo_id, local_dir=local_folder)
-print(f"Model files downloaded to: {local_folder}")
-# %%
