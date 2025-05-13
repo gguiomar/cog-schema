e1e76e43383e277d5eab0ef7cb649904da20eb49
DanielGaivaoLozano <daniel@gaivao.net>
2025-05-13 11:19:38 +0200

    Improvements to prompts (added initial prompt as system prompt). Fixed obtaining tokens where instruct models were ignored.

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def parse_args():
     parser.add_argument('--activation-layers', type=str, default='post_attention_layernorm',
                         help='Layers to save activations for activation analysis, see model.named_modules() for options')
 
-    parser.add_argument('--automate-activations-gathering', action='store_true', default=True,
+    parser.add_argument('--automate-activations-gathering', action='store_true',
                         help='Whether to automate the gathering of activations based on the layer ending. If True, activation-layers argument'
                              'will represent layer ending, e.g. post_attention_layernorm')
 
